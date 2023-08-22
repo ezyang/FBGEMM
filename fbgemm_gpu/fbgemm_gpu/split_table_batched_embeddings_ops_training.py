1031fad0df514fba33975d5c32377877df872e15
Edward Z. Yang <ezyang@meta.com>
2023-08-22 08:12:31 -0700

    blaze
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>

--- fbgemm_gpu/fbgemm_gpu/split_table_batched_embeddings_ops_training.py
+++ fbgemm_gpu/fbgemm_gpu/split_table_batched_embeddings_ops_training.py
@@ -916,6 +916,7 @@ class SplitTableBatchedEmbeddingBagsCodegen(nn.Module):
             )
 
         (indices, offsets) = indices.long(), offsets.long()
+        """
         if self.bounds_check_mode_int != BoundsCheckMode.NONE.value:
             torch.ops.fbgemm.bounds_check_indices(
                 self.rows_per_table,
@@ -927,7 +928,7 @@ class SplitTableBatchedEmbeddingBagsCodegen(nn.Module):
                 B_offsets=vbe_metadata.B_offsets,
                 max_B=vbe_metadata.max_B,
             )
-        self.step += 1
+        """
         if len(self.timesteps_prefetched) == 0:
             self._prefetch(indices, offsets)
 
